365e770695bdc776d7e9029bd8437ee396cb0ab9
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-09-13 18:48:31 +0200

    added writeKey method

--- controller/generic_device.cpp
+++ controller/generic_device.cpp
@@ -1,45 +1,152 @@
 #include "generic_device.h"
+GenericDevice::GenericDevice() {
+//    for(int i=0; i<threadPool.getNumOfThreads(); i++) {
+//        threads.push_back(std::move(std::thread(&knet::threadPool::inifiniteLoop, &threadPool)));
+//    }
+}
+GenericDevice::kernReturnValue GenericDevice::readKey(std::mutex& mtx, const char* key) {
+    SMCVal_t val;
+    kern_return_t result;
+    result = SMCReadKey(mtx,key, &val);
+//    std::cout<<"\nBytes: "<<val.bytes<<std::endl;
+//    std::cout<<"\nData Type: "<<val.dataType<<std::endl;
+//    std::cout<<"\nData size: "<<val.dataSize<<std::endl;
+//    std::cout<<"\nKey: "<<val.key<<std::endl;
 
-GenericDevice::kernReturnValue GenericDevice::readKey() {
-        SMCVal_t val;
-        kern_return_t result;
-        result = SMCReadKey(currentKey, &val);
-        kernReturnValue value;
 
+    kernReturnValue value;
     if (result == kIOReturnSuccess) {
-            // read succeeded - check returned value
-            if (val.dataSize > 0) {
-                if (strcmp(val.dataType, DATATYPE_FPE2) == 0) {
-                    // convert fpe2 value to int
-                    value.i = Converter::fpe2ToInteger(val.bytes);
-                    return value;
-                }
-                else if (strcmp(val.dataType, DATATYPE_FLT) == 0) {
-                    // convert flt value to float
-                    value.f = Converter::fltToFloat((unsigned char*) val.bytes);
-                    return value;
-                }
-                else if (strcmp(val.dataType, DATATYPE_SP78) == 0) {
-                    // convert sp78 value
-                    value.i = Converter::sp78ToInteger(val.bytes);
-                    value.i /= 4.0;
-                    return value;
-                }
-                else if(strcmp(val.dataType, DATATYPE_UINT16) == 0) {
-                    value.i = abs(Converter::ui16ToInteger((unsigned char *)val.bytes));
-                    return value;
-                }
-
+        // read succeeded - check returned value
+        if (val.dataSize > 0) {
+            if (strcmp(val.dataType, DATATYPE_FPE2) == 0) {
+                // convert fpe2 value to int
+                value.i = Converter::fpe2ToInteger(val.bytes);
+                return value;
             }
+            else if (strcmp(val.dataType, DATATYPE_SP78) == 0) {
+                // convert sp78 value
+                value.i = Converter::sp78ToInteger(val.bytes);
+//                    value.i /= 4.0;
+                return value;
+            }
+            else if (strcmp(val.dataType, DATATYPE_FLT) == 0) {
+                // convert flt value to float
+                value.f = Converter::fltToFloat((unsigned char*) val.bytes);
+                std::cout<<"Value: " <<value.f << std::endl;
+                return value;
+            }
+            else if(strcmp(val.dataType, DATATYPE_UINT16) == 0) {
+                value.i = abs(Converter::ui16ToInteger((unsigned char *)val.bytes));
+                return value;
+            } else if (strcmp(val.dataType, DATATYPE_SP96) == 0) {
+                value.f = val.bytes[0] << 8 | val.bytes[1];
+                return value;
+            } else if(strcmp(val.dataType, DATATYPE_UINT32) == 0) {
+
+                value.i = Converter::ui32ToInteger(val.bytes);
+                std::cout<<"HERE: "<<value.i<<std::endl;
+                return value;
+            } else {
+                throw std::invalid_argument("Unknown type.\n");
+            }
+        } else
+            throw std::invalid_argument("Unknown SMC Key.\n");
+    } else
+        throw std::invalid_argument("SMC Key read failed.\n");
+
+    // read failed
+    value.i = -1.0;
+    return value;
+}
+
+void GenericDevice::writeKey(std::mutex &mtx, SMCVal_t *writeValue) {
+    kern_return_t result = SMCWriteKey(writeValue);
+    if(result == kIOReturnSuccess)
+        std::cout<<"SUCCESS\n";
+    else
+        std::cout<<"SHit happens\n";
+
+}
+
+GenericDevice::kernReturnValue GenericDevice::readKey(const char *key) {
+    SMCVal_t val;
+    kern_return_t result;
+    result = SMCReadKey(key, &val);
+//    std::cout<<"\nBytes: "<<val.bytes<<std::endl;
+//    std::cout<<"\nData Type: "<<val.dataType<<std::endl;
+//    std::cout<<"\nData size: "<<val.dataSize<<std::endl;
+//    std::cout<<"\nKey: "<<val.key<<std::endl;
+
+
+    kernReturnValue value;
+    if (result == kIOReturnSuccess) {
+        // read succeeded - check returned value
+        if (val.dataSize > 0) {
+            if (strcmp(val.dataType, DATATYPE_FPE2) == 0) {
+                // convert fpe2 value to int
+                value.i = Converter::fpe2ToInteger(val.bytes);
+                return value;
+            }
+            else if (strcmp(val.dataType, DATATYPE_SP78) == 0) {
+                // convert sp78 value
+                value.i = Converter::sp78ToInteger(val.bytes);
+//                    value.i /= 4.0;
+                return value;
+            }
+            else if (strcmp(val.dataType, DATATYPE_FLT) == 0) {
+                // convert flt value to float
+                value.f = Converter::fltToFloat((unsigned char*) val.bytes);
+                std::cout<<"Value: " <<value.f << std::endl;
+                return value;
+            }
+            else if(strcmp(val.dataType, DATATYPE_UINT16) == 0) {
+                value.i = abs(Converter::ui16ToInteger((unsigned char *)val.bytes));
+                return value;
+            } else if (strcmp(val.dataType, DATATYPE_SP96) == 0) {
+                value.f = val.bytes[0] << 8 | val.bytes[1];
+                return value;
+            } else if(strcmp(val.dataType, DATATYPE_UINT32) == 0) {
+
+                value.i = Converter::ui32ToInteger(val.bytes);
+                std::cout<<"HERE: "<<value.i<<std::endl;
+                return value;
+            } else if (strcmp(val.dataType, DATATYPE_CH8) == 0) {
+                std::cout<<"BYTES: " << val.bytes<<std::endl;
+            } else if (strcmp(val.dataType, DATATYPE_UINT8) == 0) {
+                std::cout<<"BYTES: " << (int)val.bytes[0]<<std::endl;
+            }else if (strcmp(val.dataType, DATATYPE_HEX) == 0) {
+                std::cout<<"BYTES: " << val.bytes<<std::endl;
+            }
+
+            else {
+                std::cout<<val.dataType<<std::endl;
+//                throw std::invalid_argument("Unknown type.\n");
+            }
+        } else
+        {
+                        throw std::invalid_argument("Unknown SMC Key.\n");
+
+
         }
-        // read failed
-        value.i = -1.0;
-        return value;
+    } else
+        throw std::invalid_argument("SMC Key read failed.\n");
+
 }
 
-bool GenericDevice::writeKey() {
-    return false;
+void GenericDevice::writeKey(const char *key, const SMCBytes_t value) {
+    SMCVal_t writeVal;
+    strncpy(writeVal.key, key, 5);
+    for(int i =0 ; i< 32; i++)
+        writeVal.bytes[i] = value[i];
+
+//    memcpy(writeVal.bytes, value, 32);
+    strncpy(writeVal.dataType, DATATYPE_CH8, 32);
+    writeVal.dataSize = 4;
+
+    kern_return_t result = SMCWriteKey(writeVal);
+    if(result == kIOReturnSuccess)
+        std::cout<<"SUCCESS\n";
+    else
+        std::cout<<"SHit happens\n";
+
 }
-
-
-
--- controller/generic_device.h
+++ controller/generic_device.h
@@ -5,23 +5,38 @@
 #include <IOKit/IOKitLib.h>
 #include "smc.h"
 #include "Utils/utilsConverter.h"
+#include "Utils/threadPool.h"
+
+template <size_t size>
+using keyContainer = std::array<const char [5], size>;
 
 class GenericDevice {
+
+public:
+    GenericDevice();
+
+
+
+    typedef union {
+        float f;
+        int i;
+    } kernReturnValue;
+
+    kernReturnValue readKey(std::mutex & mtx, const SMC_KEY key = nullptr);
+    kernReturnValue readKey(const SMC_KEY key = nullptr);
+
+
+        void writeKey(std::mutex &mtx, SMCVal_t *value);
+        void writeKey(const SMC_KEY key, const SMCBytes_t value);
 protected:
-        std::string name;
-        char currentKey[5];
 
-    protected:
+    std::string name;
+    SMC_KEY currentKey{};
+    kernReturnValue smcKeyValue{};
+//    std::vector<std::thread> threads;
+    knet::threadPool threadPool;
+//    virtual void setCurrentKey(, const SMC_KEY key);
 
-    public:
-
-        typedef union {
-            float f;
-            int i;
-        } kernReturnValue;
-
-        kernReturnValue readKey();
-        bool writeKey();
 };
 
 
