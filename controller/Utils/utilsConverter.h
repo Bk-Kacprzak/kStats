1ea45a5e30cd52925a83d93c86c4f60f6ab8f01b
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-09-13 18:57:16 +0200

    added new converter methods
    float to flt
    string to unsigned long
    Celcius to Fahrenheit

--- controller/Utils/utilsConverter.h
+++ controller/Utils/utilsConverter.h
@@ -1,14 +1,19 @@
 #ifndef CPUSTATS_UTILSCONVERTER_H
 #define CPUSTATS_UTILSCONVERTER_H
+
+#include "../smc.h"
+
 class Converter {
 public:
-    inline static void ultostr(char* str, UInt32 val);
+    inline static UInt32 strToUl(const char* str, int size, int base);
+    inline static void ulToStr(char* str, UInt32 val);
     inline static float fltToFloat(const unsigned char *str);
+    inline static void floatToFlt(float str, SMCBytes_t& returnBuffer);
     inline static int  ui16ToInteger(const unsigned char* str);
     inline static int sp78ToInteger(const SMCBytes_t bytes);
     inline static int fpe2ToInteger(const SMCBytes_t bytes);
     inline static int ui32ToInteger(const SMCBytes_t bytes);
-
+    inline static double convertToFahrenheit(const double& celsius);
     template<typename convertedType, typename typeToConvert, size_t size>
     union template_union{
         convertedType value;
@@ -17,7 +22,20 @@ public:
 
 };
 
-void Converter::ultostr(char *str, UInt32 val) {
+
+UInt32 Converter::strToUl(const char *str, int size, int base) {
+    UInt32 total = 0;
+    for (int i = 0; i < size; i++) {
+        if (base == 16)
+            total += str[i] << (size - 1 - i) * 8;
+        else
+            total += (unsigned char)(str[i] << (size - 1 - i) * 8);
+    }
+
+    return total;
+}
+
+void Converter::ulToStr(char *str, UInt32 val) {
     str[0] = '\0';
     sprintf(str, "%c%c%c%c",
             (unsigned int)val >> 24,
@@ -51,12 +69,33 @@ int Converter::fpe2ToInteger(const char *bytes) {
 int Converter::ui32ToInteger(const char *bytes) {
 
     //if little endian
-    return ((unsigned char) bytes[0] << 24) |
-           ((unsigned char) bytes[1] << 16) |
-           ((unsigned char) bytes[2] << 8) |
-           ((unsigned char) bytes[3]);
+    if(BYTE_ORDER == LITTLE_ENDIAN)
+        return ((unsigned char) bytes[0] << 24) |
+               ((unsigned char) bytes[1] << 16) |
+               ((unsigned char) bytes[2] << 8) |
+               ((unsigned char) bytes[3]);
+    else
+        return
+                ((unsigned char) bytes[0]) |
+                ((unsigned char) bytes[1]) |
+                ((unsigned char) bytes[2]) |
+                ((unsigned char) bytes[3]);
 
+}
 
+double Converter::convertToFahrenheit(const double &celsius) {
+    return (celsius * (9.0 / 5.0)) + 32.0;
+}
+
+void Converter::floatToFlt(float str, SMCBytes_t& returnBuffer) {
+    template_union<float, unsigned char, 32> x;
+    memset(&x.buffer, 0, 32);
+    \
+    x.value = str;
+    std::cout<<"x.buffer: "<<x.buffer<<std::endl;
+    memcpy(returnBuffer,(signed char *)x.buffer, 32);
+
+    std::cout<<"returnBuffer: "<<returnBuffer<<std::endl;
 }
 
 
