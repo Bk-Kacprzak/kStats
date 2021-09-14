6d68dab8d690394b5a9cb44956bba39f9c375353
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-09-14 16:06:30 +0200

    need to refactor interface for getters

--- controller/cpu_device.cpp
+++ controller/cpu_device.cpp
@@ -1,6 +1,112 @@
-
 #include "cpu_device.h"
+#include "Utils/utils.h"
+#include <thread>
+#include <vector>
+CPU::CPU() = default;
 
-CPU::CPU() {
+void CPU::setKey(CPU::KEYTYPE keytype, const ushort id) {
+    switch (keytype) {
+        case TEMPERATURE:
+            memcpy(currentKey, temperature[id], 5);
+            break;
+        case POWER:
+            memcpy(currentKey, power[id], 5);
+            break;
+        case CURRENT:
+//            memcpy(currentKey, temperature[id], 5);
+            break;
+        case VOLTAGE:
+//            memcpy(currentKey, temperature[id], 5);
+            break;
+        default:
+            throw std::invalid_argument("Unkown keytype.\n");
+    }
+}
+
+void CPU::getTemperature(const int &core) {
+    try {
+//        if(core >= Utils::getArraySize(temperature))
+//            throw std::invalid_argument("Invalid core number.\n");
+//        std::lock_guard<std::mutex> lockGuard(mtx);
+        int return_value = readKey(temp_mutex,temperature[core]).i;
+
+        //consumer producer problem!!!!
+        temp_mutex.lock();
+        CPUTemperature[core] = return_value/256.0;
+
+        if(core == 6)  {
+            //avg temp
+            std::cout<<"Core average: "<<CPUTemperature[core]<<std::endl;
+        }
+        else if (core == 7) {
+            std::cout<<"Core PECI: "<<CPUTemperature[core]<<std::endl;
+        } else {
+            std::cout<<"Core "<<core + 1 <<": "<<CPUTemperature[core]<<std::endl;
+        }
+        temp_mutex.unlock();
+    }
+
+    catch (const std::exception& e) {
+       std::cout<<e.what();
+       exit(0);
+    }
 
 }
+
+void CPU::getVoltage(const int &core) {
+
+}
+
+void CPU::getPower(const int &core) {
+
+}
+
+void CPU::getCurrent(const int &core) {
+
+}
+
+void CPU::getName(const int &core) {
+
+}
+
+ushort CPU::getCoreNumber() {
+    return 6;
+}
+
+void CPU::getEachCoreTemperature() {
+    for(int i = 0; i<temperature.size(); i++) {
+        threadPool.push([=] {
+            getTemperature(i);
+        });
+    }
+
+
+//
+//    for(int i = 0 ; i< 6; i++) {
+//        if (threads[i].joinable())
+//            threads[i].join();
+//    }
+
+
+//
+//    std::thread a(&CPU::getTemperature,this, 0);
+//    std::thread b(&CPU::getTemperature,this, 1);
+//    std::thread c(&CPU::getTemperature,this, 2);
+//    std::thread d(&CPU::getTemperature,this, 3);
+//    std::thread e(&CPU::getTemperature,this, 4);
+//    std::thread f(&CPU::getTemperature,this, 5);
+//
+//    a.join();
+//    b.join();
+//    c.join();
+//    d.join();
+//    e.join();
+//    f.join();
+}
+
+CPU::~CPU() {
+
+}
+
+
+
--- controller/cpu_device.h
+++ controller/cpu_device.h
@@ -1,46 +1,32 @@
 #ifndef CPUSTATS_CPU_DEVICE_H
 #define CPUSTATS_CPU_DEVICE_H
 
-#define SMC_KEY_CPU_TEMP_CORE_AVG "TC0P" //AVERAGE
-#define SMC_KEY_CPU_TEMP_CORE_PECI "TCXC" //PECI
-#define SMC_KEY_CPU_TEMP_CORE1 "TC1C" //CORE 1
-#define SMC_KEY_CPU_TEMP_CORE2 "TC2C" //CORE 2
-#define SMC_KEY_CPU_TEMP_CORE3 "TC3C" //CORE 3
-#define SMC_KEY_CPU_TEMP_CORE4 "TC4C" //CORE 4
-#define SMC_KEY_CPU_TEMP_CORE5 "TC5C" //CORE 5
-#define SMC_KEY_CPU_TEMP_CORE6 "TC6C" //CORE 6
-#define SMC_KEY_CPU_TEMP_CORE7 "PC6C"
-#define SMC_KEY_CPU_TEMP_CORE8 "PC7C"
-
-
-#define SMC_KEY_CPU_POWER_CORE1 "PC0C"
-#define SMC_KEY_CPU_POWER_CORE2 "PC1C"
-#define SMC_KEY_CPU_POWER_CORE3 "PC2C"
-#define SMC_KEY_CPU_POWER_CORE4 "PC3C"
-#define SMC_KEY_CPU_POWER_CORE5 "PC4C"
-#define SMC_KEY_CPU_POWER_CORE6 "PC5C"
-#define SMC_KEY_CPU_POWER_CORE7 "PC6C"
-#define SMC_KEY_CPU_POWER_CORE8 "PC7C"
-
-
 #include "generic_device.h"
-
+#include "Utils/threadPool.h"
+#include "Utils/keys.h"
+#include <mutex>
+#include <vector>
 class CPU : public GenericDevice {
 private:
-    static constexpr char temperature[10][5] = {
+    static constexpr keyContainer<10> temperature = {
             SMC_KEY_CPU_TEMP_CORE1,
             SMC_KEY_CPU_TEMP_CORE2,
             SMC_KEY_CPU_TEMP_CORE3,
             SMC_KEY_CPU_TEMP_CORE4,
             SMC_KEY_CPU_TEMP_CORE5,
             SMC_KEY_CPU_TEMP_CORE6,
-            SMC_KEY_CPU_TEMP_CORE7,
-            SMC_KEY_CPU_TEMP_CORE8,
+//            SMC_KEY_CPU_TEMP_CORE7,
+//            SMC_KEY_CPU_TEMP_CORE8,
             SMC_KEY_CPU_TEMP_CORE_AVG,
-            SMC_KEY_CPU_TEMP_CORE_PECI
+            SMC_KEY_CPU_TEMP_CORE_PECI,
+            SMC_KEY_CPU_TEMP_CORE6,
+            SMC_KEY_CPU_TEMP_CORE6,
     };
+//    static constexpr char temperature[10][5] = {
+//
+//    };
 
-    static constexpr char power[8][5] = {
+    static constexpr keyContainer<8> power = {
             SMC_KEY_CPU_POWER_CORE1,
             SMC_KEY_CPU_POWER_CORE2,
             SMC_KEY_CPU_POWER_CORE3,
@@ -51,15 +37,33 @@ private:
             SMC_KEY_CPU_POWER_CORE8,
     };
 
+    std::mutex temp_mutex;
+    //concurrency
+//    std::vector<std::thread> threads;
+//    knet::threadPool threadPool;
+    std::array<float, 8> CPUTemperature = { 0,0,0,0,0,0,0,0};
+
+    //?????
 public:
 
+    enum KEYTYPE {
+        TEMPERATURE = 0,
+        POWER = 1,
+        CURRENT = 2,
+        VOLTAGE = 3,
+    };
+
     CPU();
+    ~CPU();
     //kernel getters
     void getTemperature(const int& core);
+    void getEachCoreTemperature();
     void getVoltage(const int& core);
     void getPower(const int& core);
     void getCurrent(const int& core);
     void getName(const int& core);
+    void setKey(KEYTYPE,const ushort id);
     ushort getCoreNumber();
+
 };
 #endif //CPUSTATS_CPU_DEVICE_H
