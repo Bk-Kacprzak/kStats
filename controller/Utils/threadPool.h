067caad5ee3dca3eb311de58901a21faef893b98
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-10-06 11:34:36 +0200

    little clean code corrections

--- controller/Utils/threadPool.h
+++ controller/Utils/threadPool.h
@@ -6,7 +6,6 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#include <cassert>
 #include <thread>
 
 
@@ -23,14 +22,15 @@ namespace knet {
     public:
         threadPool();
         ~threadPool();
+        threadPool(const threadPool&) = delete;
+        threadPool(const threadPool&&) = delete;
+        threadPool& operator=(const threadPool &) = delete;
+
         int getNumOfThreads() const;
         void push(std::function<void()> func);
         void exit();
         void inifiniteLoop();
     };
 }
-
-
-
 
 #endif //CPUSTATS_THREADPOOL_H
