c3f5eaa413ca2b077dde9789ca449b7fd44230e8
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-10-19 21:16:51 +0200

    Qt libraries for GUI

---
+++ view/qt_libraries.h
@@ -0,0 +1,20 @@
+#ifndef KSTATS_VIEW_H_QT_LIBRARIES_H
+#define KSTATS_VIEW_H_QT_LIBRARIES_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+#endif //KSTATS_VIEW_H_QT_LIBRARIES_H
