bb44dbe41e85895880d5ff5ee2a1772ad8b02213
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2021-10-19 21:09:31 +0200

    file containing application interface instances

---
+++ global.h
@@ -0,0 +1,8 @@
+#ifndef KSTATS_VIEW_H_GLOBAL_H
+#define KSTATS_VIEW_H_GLOBAL_H
+#include "controller/kStatsController.h"
+#include "view/kStats_view.h"
+#include "model/Utils/threadPool.h"
+extern kStatsController ApplicationController;
+extern kStatsView ApplicationView;
+#endif //KSTATS_VIEW_H_GLOBAL_H
