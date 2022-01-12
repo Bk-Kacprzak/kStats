a216033b3cac3b314f147c49d1fda3af014c934d
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-01-12 21:44:28 +0100

    Add new keys for bluetooth devices
    Mac OS Monterey brings new codes to recognize a type of specific device (keyboard and mouse)

--- model/Peripherals/generic_peripheral.h
+++ model/Peripherals/generic_peripheral.h
@@ -9,18 +9,29 @@ class GenericPeripheral {
     BluetoothClassOfDevice _deviceType;
 public:
     enum  {
-        KEYBOARD = 1344,
-        MOUSE = 1408,
+        KEYBOARD = 9536,
+        MOUSE = 9600,
         HEADPHONES = 2360344,
         SCREEN = 1001
     };
+//MAC OS MONTEREY:
+//    KEYBOARD = 9536
+//    MOUSE = 9600
+//    HEADPHONES = 2360344
+
+//MAC OS BIG SUR
+//        KEYBOARD = 1344,
+//        MOUSE = 1408,
+//        HEADPHONES = 2360344,
+//        SCREEN = 1001
+
     GenericPeripheral(const std::string& _name, const BluetoothClassOfDevice& _deviceType);
     GenericPeripheral(GenericPeripheral&& _obj) noexcept ;
     GenericPeripheral(const GenericPeripheral& _obj);
     GenericPeripheral& operator=(GenericPeripheral&& _obj);
     ~GenericPeripheral() {}
     const std::string& name() const;
-    std::string deviceType() const;
+    const std::string deviceType() const;
 };
 
 #endif //CPUSTATS_GENERIC_PERIPHERAL_H
