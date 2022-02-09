52c84ad92d6be0dd9bb07666ef951a360dc67142
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-02-09 16:35:47 +0100

    add animation to connection speed test

--- view/animation.h
+++ view/animation.h
@@ -1,16 +1,8 @@
 #ifndef KSTATS_VIEW_H_ANIMATION_H
 #define KSTATS_VIEW_H_ANIMATION_H
-
 #include "qt_libraries.h"
-
 #define MIN_TEMPERATURE 60
 #define MAX_TEMPERATURE 80
-
-//constexpr std::array<double, 3> blue{71,152,255};
-//constexpr std::array<double, 3> red{248,47,95};
-//constexpr std::array<double, 3> incrementIndex{(red[0] - blue[0])/(MAX_TEMPERATURE-MIN_TEMPERATURE),
-//                                             (red[1] - blue[1])/(MAX_TEMPERATURE-MIN_TEMPERATURE),
-//                                             (red[2] - blue[2])/(MAX_TEMPERATURE-MIN_TEMPERATURE)};
 
 
 constexpr QColor _blue(71,152,255);
@@ -21,8 +13,6 @@ const auto blueStyleSheet = QString::fromUtf8("QFrame { \nborder-radius: 70px;\n
 const auto orangeStyleSheet = QString::fromUtf8("QFrame { \nborder-radius: 70px;\nbackground-color: qconicalgradient(cx:0.5 cy:0.5 angle:225, stop: 0.1 rgba(255, 255, 255, 0), stop:1 ") + _orange.name() + QString(")};");
 const auto redStyleSheet = QString::fromUtf8("QFrame { \nborder-radius: 70px;\nbackground-color: qconicalgradient(cx:0.5 cy:0.5 angle:225, stop: 0.1 rgba(255, 255, 255, 0), stop:1 ") + _red.name() + QString(")};");
 
-static std::mutex animationMtx;
-
 static class Animation {
 
 public:
@@ -32,18 +22,32 @@ public:
             styleSheet = charts[i]->styleSheet();
             if(temperatures[i] <= MIN_TEMPERATURE) {
                 if (!styleSheet.contains(_blue.name(), Qt::CaseInsensitive)) {
-                    std::lock_guard<std::mutex> mtx(animationMtx);
                     charts[i]->setStyleSheet(blueStyleSheet);
                 }
             }
             else if(temperatures[i] > MIN_TEMPERATURE && temperatures[i] < MAX_TEMPERATURE) {
                 if (!styleSheet.contains(_orange.name(), Qt::CaseInsensitive)) {
-                    std::lock_guard<std::mutex> mtx(animationMtx);
                     charts[i]->setStyleSheet(orangeStyleSheet);
                 }
             } else
             if(!styleSheet.contains(_red.name(),Qt::CaseInsensitive)){
                 charts[i]->setStyleSheet(redStyleSheet);
+            }
+        }
+    }
+
+    static void networkAnimation(QLabel* label, const QString& content) {
+        using namespace std::chrono_literals;
+        QString dot;
+        while(content.isEmpty()) {
+            dot="";
+
+            for(int i = 0; i<3; i++) {
+                dot+=".";
+                if(content.isEmpty())
+                    label->setText(dot);
+
+                std::this_thread::sleep_for(500ms);
             }
         }
     }
